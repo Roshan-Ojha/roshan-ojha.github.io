07805ce64e66160ef4a933df9f596ad499090199
Roshan-Ojha <roshanojha65@gmail.com>
2023-10-19 00:40:16 +0545

    pls

--- app/page.tsx
+++ app/page.tsx
@@ -13,13 +13,13 @@ export default function Home() {
       <Navbar page="home"></Navbar>
       
       <div className="mt-[130px] flex flex-row justify-center space-x-[100px] max-[1110px]:space-x-[0px] max-[1110px]:flex-col max-[1110px]:mt-[25px]">
-        <div className="mt-[135px] flex w-[500px] flex-col max-[1110px]:self-center max-[1110px]:mt-[10px] max-[1110px]:w-[420px]">
+        <div className="mt-[135px] flex w-[500px] flex-col max-[1110px]:self-center max-[1110px]:mt-[10px] max-[1110px]:w-[350px]">
           <div className="text-[50px] font-[475] leading-[50px] text-[#212121]">
             <span className="max-[1110px]:hidden">I&apos;m</span> Roshan&nbsp;
             <br className="max-[1110px]:hidden"></br>
             Ojha
           </div>
-          <div className="mt-[20px] text-[#6c6c6b] max-[1110px]: ">
+          <div className="mt-[20px] text-[#6c6c6b] max-[1110px]:w-[350px]">
             My Portfolio shows off my abilities in frontend devlopment using
             React.js and my exploration of fullstack devlopment with next.js,
             featuring a variety of projects from Hackathons, competions, and
